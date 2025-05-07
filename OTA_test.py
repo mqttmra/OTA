e71d9c63c9002e99324e24a795e8b4092e5b4460
mqttmra <91822787+mqttmra@users.noreply.github.com>
2025-05-07 21:33:49 +0200

    Erster Versuch

--- OTA_test.py
+++ OTA_test.py
@@ -1,8 +1,8 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://github.com/kevinmcaleer/ota_test/main/"
+firmware_url = "https://github.com/mqttmra/OTA/test.py"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test_ota.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
 
 ota_updater.download_and_install_update_if_available()
